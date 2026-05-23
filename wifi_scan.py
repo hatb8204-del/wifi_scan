b90881e164617eae6466f37967f88daebf654f04
hatb8204-del <hatb8204@gmail.com>
2026-05-23 18:25:11 +0000

    wifi_scan.py
    Wifi scanner

---
+++ wifi_scan.py
@@ -0,0 +1,9 @@
+import os
+
+print("======================")
+print("     ASAYUU WIFI")
+print("======================")
+
+print("Scanning nearby Wi-Fi networks...\n")
+
+os.system("termux-wifi-scaninfo")
